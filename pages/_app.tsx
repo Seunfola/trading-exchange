ed57a3bfbd215ebf3f64be7fb6ad9c4719c786c4
Seunfola <seunfola1@gmail.com>
2024-12-12 22:13:56 +0100

    added the provider to the root app folder for the wallet creation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,17 @@ import { AppProps } from 'next/app';
 import { AuthProvider } from '../context/AuthContext';
 import Layout from './layout'; 
 import '../styles/globals.css';
+import { WalletProvider } from '../context/WalletContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+    <WalletProvider>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </AuthProvider>
+    </WalletProvider>
+  </AuthProvider>
   );
 }
 
